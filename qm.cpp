428eb4bafcc8a9d74bf9bcd5d12cb79adbb1bd4c
Rift2020 <69626219+Rift2020@users.noreply.github.com>
2021-09-26 22:10:30 +0800

    Add files via upload

--- qm.cpp
+++ qm.cpp
@@ -1,7 +1,7 @@
 /*
 
  将逻辑表达式的所有取值打表输出，并且使用不完全的quine mccluskey算法找出一个稍简单的等价表达式
- 不完全的意思是它缺少素项表部分，因此得到的表达式很有可能不是最简表达式
+ 不完全的意思是它缺少素项表部分，因此得到的表达式通常都不是最简表达式
 
  一时兴起的为了实现出来玩一玩
  一天闲暇时间写出来的垃圾东西，算法性能也不好（主要是常数不好），可读性也烂的一批，维护性完全没有的小辣鸡代码
@@ -13,6 +13,8 @@
 #include <stack>
 #include <unordered_map>
 #include <vector>
+#include<algorithm>
+#include<string.h>
 using namespace std;
 //运算符优先级
 unordered_map<char,int> priority={{'!',5},{'&',4},{'|',3},{'>',4},{'=',5}};
